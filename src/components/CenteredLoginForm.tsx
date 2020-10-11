90789870eddf1bd1b90906d1701a21b507180010
MarioJim <mario.emilio.j@gmail.com>
2020-10-11 16:05:09 -0500

    Added login and forgot password inputs

---
+++ src/components/CenteredLoginForm.tsx
@@ -0,0 +1,12 @@
+import styled from '@emotion/styled';
+
+const CenteredLoginForm = styled.form`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 20px;
+`;
+
+export default CenteredLoginForm;
