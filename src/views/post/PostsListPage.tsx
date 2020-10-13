2d401153f51cda5deb88adc0f81c1d1f229fc77f
MarioJim <mario.emilio.j@gmail.com>
2020-10-13 00:20:26 -0500

    Posts and Resident List query from src/firebase

--- src/views/post/PostsListPage.tsx
+++ src/views/post/PostsListPage.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import Markdown from 'markdown-to-jsx';
 
-const PostsListPage: React.FC = () => (
-  <div>
-    <h1>Posts List Page</h1>
-  </div>
-);
+import { getPosts } from '../../firebase/db/posts';
+
+const PostsListPage: React.FC = () => {
+  const [posts, setPosts] = useState<Post[]>([]);
+  useEffect(() => {
+    getPosts().then((ps) => setPosts(ps));
+  });
+  return (
+    <div>
+      <h1>Posts List Page</h1>
+      {posts.map((p) => (
+        <div key={p._id}>
+          <img
+            alt={`Imagen para el post "${p.title}"`}
+            src={p.imageUrl}
+            height="200px"
+          />
+          <h1>{p.title}</h1>
+          <Markdown>{p.content}</Markdown>
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default PostsListPage;
--- src/views/resident/ResidentListPage.tsx
+++ src/views/resident/ResidentListPage.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const ResidentListPage: React.FC = () => (
-  <div>
-    <h1>Resident List Page</h1>
-  </div>
-);
+import { getResidents } from '../../firebase/db/resident';
+
+const ResidentListPage: React.FC = () => {
+  const [residents, setResidents] = useState<Resident[]>([]);
+  useEffect(() => {
+    getResidents().then((resid) => setResidents(resid));
+  });
+  return (
+    <div>
+      <h1>Resident List Page</h1>
+      {residents.map((r) => (
+        <div key={r._id}>
+          <h2>
+            Nombre: {r.firstName} {r.lastName}
+          </h2>
+          <h5>Edad: {r.age}</h5>
+          <h5>Sexo: {r.gender}</h5>
+          {r.isVisible || (
+            <h5>
+              Este residente no se encuentra actualmente en la institución
+            </h5>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
 
 export default ResidentListPage;
