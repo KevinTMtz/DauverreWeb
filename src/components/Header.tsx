71d6607a353de7fdb936a74782a59aef070e66f3
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-10-13 23:12:52 -0500

    Added header style.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,14 +1,39 @@
-import React from 'react';
 import { useLocation, useRouteMatch } from 'react-router-dom';
+/** @jsx jsx */ import { css, jsx } from '@emotion/core';
+import React from 'react';
+
+const divStyle = css({
+  margin: '0px',
+  borderBottom: '1px solid black',
+  padding: '8px 16px',
+  display: 'flex',
+  justifyContent: 'space-between',
+  alignItems: 'center'
+})
+
+const pStyle = css({
+  margin: '0px',
+  fontSize: '24px'
+})
+
+const buttonStyle = css({
+  backgroundColor: '#74b9ff',
+  border: '0px solid black',
+  borderRadius: '10px',
+  fontSize: '18px',
+  padding: '5px 10px'
+})
 
 const Header: React.FC = () => {
   const location = useLocation();
   console.log(location);
   const match = useRouteMatch();
   console.log(match);
+  
   return (
-    <div>
-      <h1>Header</h1>
+    <div css={divStyle}>
+      <p css={pStyle}>Dauverre Web</p>
+      <button css={buttonStyle}>Iniciar sesión</button>
     </div>
   );
 };
