2928cbd13b31f5c3def0871733d40c46e790662e
MarioJim <mario.emilio.j@gmail.com>
2020-10-23 02:27:22 -0500

    Start functions createResAcc and resetPassword

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,24 @@
 import * as functions from 'firebase-functions';
+import * as admin from 'firebase-admin';
+import * as express from 'express';
+import * as cors from 'cors';
+import { json } from 'body-parser';
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+import createResidentFamiliarAccount from './createResidentFamiliarAccount';
+import { logger } from './util';
+import resetPasswordResidentFamAcc from './resetPasswordResidentFamAcc';
+
+admin.initializeApp();
+
+const app = express();
+app.use(json());
+app.use(cors({ origin: true }));
+app.use(logger);
+
+app.use('/users/reset/:uid', resetPasswordResidentFamAcc);
+
+export const api = functions.https.onRequest(app);
+
+export const createResidentsRelativeAccount = functions.firestore
+  .document('residents/{residentID}')
+  .onCreate(createResidentFamiliarAccount);
---
+++ functions/src/resetPasswordResidentFamAcc.ts
@@ -0,0 +1,28 @@
+import { Request, Response } from 'express';
+import { logger } from 'firebase-functions';
+import * as admin from 'firebase-admin';
+
+import { Resident } from './types';
+import { dateToPass } from './util';
+
+const resetPasswordResidentFamAcc = async (req: Request, res: Response) => {
+  try {
+    const uid: string = req.params.uid;
+    const residentDoc = await admin.firestore().doc(`residents/${uid}`).get();
+    if (!residentDoc.exists) {
+      return res
+        .status(200)
+        .send({ error: `El documento con id ${uid} no existe` });
+    }
+    const { birthDate } = residentDoc.data() as Resident;
+    await admin.auth().updateUser(uid, {
+      password: dateToPass(birthDate.toDate()),
+    });
+    logger.info(`Reset resident relative account password with uid ${uid}`);
+    return res.status(200).send({ success: true });
+  } catch (err) {
+    return res.status(200).send({ error: err.message, errcode: err.code });
+  }
+};
+
+export default resetPasswordResidentFamAcc;
