168b3bdbc1b6dc435448cfff98cf670af22a41e6
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-10-23 00:04:12 -0500

    Start Page Elements & Posts. (#2)

---
+++ src/components/Divisor.tsx
@@ -0,0 +1,16 @@
+/** @jsx jsx */ import { css, jsx } from '@emotion/core';
+import React from 'react';
+
+const divisorStyle = css({
+    height: '2vh',
+    backgroundColor: '#74b9ff'
+  })
+
+const Post: React.FC = () => {
+  return (
+    <div css={divisorStyle}>
+    </div>
+  );
+};
+
+export default Post;
