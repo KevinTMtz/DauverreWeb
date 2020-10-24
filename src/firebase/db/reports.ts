2e273c3d6e43738401be1226a4831105ffaab043
MarioJim <mario.emilio.j@gmail.com>
2020-10-24 14:16:10 -0500

    Added dummy reports functions

--- src/firebase/db/reports.ts
+++ src/firebase/db/reports.ts
@@ -1,6 +1,9 @@
+import { reportDocSchema } from './validation';
+
 const reports: Report[] = [
   {
-    _id: 'rep1',
+    residentID: '1',
+    reportID: 'rep1',
     date: new Date(Date.now()),
     mood: 1,
     health: 4,
@@ -12,7 +15,8 @@ const reports: Report[] = [
     comments: 'Un rifado',
   },
   {
-    _id: 'rep2',
+    residentID: '2',
+    reportID: 'rep2',
     date: new Date(Date.now()),
     mood: 4,
     health: 1,
@@ -24,7 +28,8 @@ const reports: Report[] = [
     comments: 'Un rifado',
   },
   {
-    _id: 'rep3',
+    residentID: '1',
+    reportID: 'rep3',
     date: new Date(Date.now()),
     mood: 1,
     health: 3,
@@ -36,7 +41,8 @@ const reports: Report[] = [
     comments: 'Un rifado',
   },
   {
-    _id: 'rep4',
+    residentID: '2',
+    reportID: 'rep4',
     date: new Date(Date.now()),
     mood: 5,
     health: 5,
@@ -49,6 +55,60 @@ const reports: Report[] = [
   },
 ];
 
-export const getReports = async (_residentID: string): Promise<Report[]> => {
-  return reports.filter((_rep) => Math.random() > 0.5);
+export const getReports = async (residentID: string): Promise<Report[]> => {
+  return reports.filter((rep) => rep.residentID === residentID);
+};
+
+export const getReport = async (
+  residentID: string,
+  reportID: string,
+): Promise<Report | NotFoundError> => {
+  const report = reports
+    .filter((rep) => rep.residentID === residentID)
+    .find((r) => r.reportID === reportID);
+  return report ? report : { notFound: true };
+};
+
+export const createReport = async (
+  reportData: ReportData,
+): Promise<SuccessMessage | ValidationErrors> => {
+  try {
+    const validatedReport = await reportDocSchema.validate(reportData);
+    console.log('Creating report:', validatedReport);
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      return error.errors;
+    }
+  }
+  return { success: true };
+};
+
+export const updateReport = async (
+  report: Report,
+): Promise<SuccessMessage | NotFoundError> => {
+  try {
+    const { residentID, reportID } = report;
+    const validatedReport = await reportDocSchema.validate(report);
+    console.log(
+      'Updating report',
+      reportID,
+      'from resident ',
+      residentID,
+      'with info',
+      validatedReport,
+    );
+  } catch (error) {
+    if (error.name === 'ValidationError') {
+      return error.errors;
+    }
+  }
+  return { success: true };
+};
+
+export const deleteReport = async (
+  residentID: string,
+  reportID: string,
+): Promise<SuccessMessage | NotFoundError> => {
+  console.log('Deleting report', reportID, 'from resident', residentID);
+  return { success: true };
 };
