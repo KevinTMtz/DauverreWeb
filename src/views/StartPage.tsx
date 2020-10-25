15c5e473733b9da6779c3d1395659b3355279f3a
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-10-24 21:10:40 -0500

    Start Page now renders dynamically. (#8)

--- src/views/StartPage.tsx
+++ src/views/StartPage.tsx
@@ -1,9 +1,11 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Markdown from 'markdown-to-jsx';
 /** @jsx jsx */ import { css, jsx } from '@emotion/core';
 
 import Post from '../components/Post';
 import Divisor from '../components/Divisor';
+
+import { getPosts } from '../firebase/db/posts';
 
 const mainImgStyle = css({
   backgroundImage:
@@ -23,23 +25,23 @@ const dauverreInfo = css({
 
 const dauverreInfoImg = css({
   backgroundImage:
-    'url(https://lh3.ggpht.com/p/AF1QipPXaAOJnSq-IcfJMTqeYZFWv-yuAPaTZrYwY3Hu=s1536)',
+    'url(https://scontent-qro1-1.xx.fbcdn.net/v/t1.0-9/78873991_589158318486755_3836700633478463488_n.jpg?_nc_cat=107&ccb=2&_nc_sid=8bfeb9&_nc_ohc=sTWvmvC5QZoAX-BVlm7&_nc_ht=scontent-qro1-1.xx&oh=90a20b13b644fb4323bf96fb6898574c&oe=5FBB2145)',
   backgroundPosition: '50%',
   backgroundSize: 'cover',
   width: '50vw',
   height: 'auto',
   '@media (max-width: 550px)': {
-    height: '25vh',
+    height: '30vh',
     width: '100%',
   },
 });
 
 const dauverreInfoSubContainer = css({
-  padding: '16px',
-  width: '50vw',
+  padding: '25px',
+  width: 'calc(50% - 50px)',
   minHeight: '25vh',
   '@media (max-width: 550px)': {
-    width: '100%',
+    width: 'calc(100% - 50px)',
   },
 });
 
@@ -49,9 +51,20 @@ const postContainerStyle = css({
   justifyContent: 'space-around',
 });
 
+const publicationsDivTitle = css({
+  display: 'flex',
+  flexWrap: 'wrap',
+  justifyContent: 'space-around',
+});
+
 const StartPage: React.FC = () => {
   const missionText =
     'Lorem ipsum\n\ndolor sit amet, consectetur adipiscing elit, sed do  eiusmod tempor incididunt ut\n\n * labore\n * et dolore magna aliqua.\n\nUt enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.';
+
+  const [posts, setPosts] = useState<Post[]>([]);
+  useEffect(() => {
+    getPosts().then((ps) => setPosts(ps));
+  });
 
   return (
     <div>
@@ -65,10 +78,11 @@ const StartPage: React.FC = () => {
         <div css={dauverreInfoImg} />
       </div>
       <Divisor />
+      <h1 css={publicationsDivTitle}>Publicaciones</h1>
       <div className="postContainer" css={postContainerStyle}>
-        <Post />
-        <Post />
-        <Post />
+        {posts.map((p) => {
+          return <Post key={p.title} {...p} />;
+        })}
       </div>
       <Divisor />
     </div>
