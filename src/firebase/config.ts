0f9597188faa520016f646089132a2cfe3e825ad
Mario Jiménez <mario.emilio.j@gmail.com>
2020-10-25 13:39:16 -0600

    Added Firebase config (#10)

---
+++ src/firebase/config.ts
@@ -0,0 +1,12 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyDPm0dMe1rpGOgV6Hkgk5v6E0kujBhTiEU',
+  authDomain: 'dauverre-ac.firebaseapp.com',
+  databaseURL: 'https://dauverre-ac.firebaseio.com',
+  projectId: 'dauverre-ac',
+  storageBucket: 'dauverre-ac.appspot.com',
+  messagingSenderId: '1044841907485',
+  appId: '1:1044841907485:web:d440d2c53c1e0928c68df4',
+  measurementId: 'G-GPXZ8Z6NFR',
+};
+
+export default firebaseConfig;
