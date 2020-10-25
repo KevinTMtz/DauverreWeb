7852540e947aa3915176057202784ea8533ac0f8
Mario Jiménez <mario.emilio.j@gmail.com>
2020-10-25 13:39:16 -0600

    Added Firebase config (#10)

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,10 @@
 import React from 'react';
+import firebase from 'firebase';
 import ReactDOM from 'react-dom';
 import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import Header from './components/Header';
+import firebaseConfig from './firebase/config';
 import ForgotPasswordPage from './views/ForgotPasswordPage';
 import LoginPage from './views/LoginPage';
 import PostsRouter from './views/post';
@@ -13,37 +15,41 @@ import AccessModule from './views/AccessModule';
 import WebpageReport from './views/WebpageReport';
 import './index.css';
 
-const App: React.FC = () => (
-  <BrowserRouter>
-    <Header />
-    <Switch>
-      <Route path="/login">
-        <LoginPage />
-      </Route>
-      <Route path="/forgotpass">
-        <ForgotPasswordPage />
-      </Route>
-      <Route path="/menu">
-        <MenuPage />
-      </Route>
-      <Route path="/posts">
-        <PostsRouter />
-      </Route>
-      <Route path="/residents">
-        <ResidentsRouter />
-      </Route>
-      <Route path="/accessmodule">
-        <AccessModule />
-      </Route>
-      <Route path="/webpagereport">
-        <WebpageReport />
-      </Route>
-      <Route path="/">
-        <StartPage />
-      </Route>
-    </Switch>
-  </BrowserRouter>
-);
+const App: React.FC = () => {
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
+  return (
+    <BrowserRouter>
+      <Header />
+      <Switch>
+        <Route path="/login">
+          <LoginPage />
+        </Route>
+        <Route path="/forgotpass">
+          <ForgotPasswordPage />
+        </Route>
+        <Route path="/menu">
+          <MenuPage />
+        </Route>
+        <Route path="/posts">
+          <PostsRouter />
+        </Route>
+        <Route path="/residents">
+          <ResidentsRouter />
+        </Route>
+        <Route path="/accessmodule">
+          <AccessModule />
+        </Route>
+        <Route path="/webpagereport">
+          <WebpageReport />
+        </Route>
+        <Route path="/">
+          <StartPage />
+        </Route>
+      </Switch>
+    </BrowserRouter>
+  );
+};
 
 ReactDOM.render(
   <React.StrictMode>
