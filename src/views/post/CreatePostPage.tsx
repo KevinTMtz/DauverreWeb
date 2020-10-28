466a5cbed8223b49024a6ff8c74de13b614d1bc4
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-10-28 02:19:02 -0600

    Created post form for creation & edition of post.

--- src/views/post/CreatePostPage.tsx
+++ src/views/post/CreatePostPage.tsx
@@ -1,11 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import PageTitle from '../../components/PageTitle';
+import PostForm from '../../components/post-components/PostForm';
 
-const CreatePostPage: React.FC = () => (
-  <div>
-    <PageTitle message={'Crear publicación'} />
-  </div>
-);
+import { createPost } from '../../firebase/db/posts';
+
+const CreatePostPage: React.FC = () => {
+  const [newPostState, setNewPostState] = useState<PostData>({
+    title: '',
+    date: new Date(),
+    content: '',
+    imageUrl:
+      'https://lh5.googleusercontent.com/p/AF1QipP3Ll5USMkZfqPJRnKbH1BSFK1XGk5x2r1La6vF=s1016-k-no',
+  });
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    createPost(newPostState);
+  };
+
+  return (
+    <div>
+      <PageTitle message={'Crear publicación'} />
+      {
+        <PostForm
+          post={newPostState}
+          onSubmit={onSubmit}
+          setNewPostState={setNewPostState}
+          buttonMessage={'Crear publicación'}
+        />
+      }
+    </div>
+  );
+};
 
 export default CreatePostPage;
