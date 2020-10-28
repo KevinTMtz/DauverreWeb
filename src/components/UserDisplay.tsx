34f5acbe6434bf2921287d0ab1ee7c4f033fb51c
GinaJame <gina10111@hotmail.com>
2020-10-28 01:10:29 -0600

    AccessModule changes
    Show users

---
+++ src/components/UserDisplay.tsx
@@ -0,0 +1,95 @@
+import React, {useState, MouseEvent} from 'react';
+import Markdown from 'markdown-to-jsx';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import MuiSelect from '@material-ui/core/Select';
+import MuiMenuItem from '@material-ui/core/MenuItem';
+import { FormControl } from "@material-ui/core";
+/** @jsx jsx */ import { css, jsx } from '@emotion/core';
+
+
+const divStyle = css({
+  padding: '10px',
+  margin: '20px',
+  display:'flex',
+  maxWidth: '50vw',
+  '@media (max-width: 600px)': {
+    maxWidth: '80vw',
+  },
+  justifyContent:'space-between'
+});
+
+
+
+const h1Style = css({
+  margin: '5px 0px',
+});
+
+const UserDisplay: React.FC <{username: string, password:string, privilege:string}>= ({username, password, privilege}) => {
+    const [edit, setEdit] = useState(false);
+    const [username2, setUsername] = useState(username);
+    const [password2, setPassword] = useState(password);
+    const [confpassword2, setConfPassword] = useState('');
+    const [privilege2, setPrivilege] = useState(privilege);
+    const onSubmit = () => {
+      if(confpassword2==password2){
+        console.log('Iniciando sesión con datos:', username2, password2,privilege2);
+      }
+    };
+    return(
+        <div css={divStyle}>
+          {!edit &&
+          <Button type="submit" variant="contained" color="primary" onClick={() => { setEdit(!edit) }}>
+                    Cambiar
+          </Button>
+          }
+          { edit &&
+          <FormControl>
+              <TextField
+                  margin="normal"
+                  id="username"
+                  label={username}
+                  name="username"
+                  autoFocus
+                  value={username2}
+                  onChange={(event) => setUsername(event.target.value)}
+                />
+              <TextField
+                margin="normal"
+                name="password"
+                label="password"
+                type="password"
+                id={password}
+                value={password2}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <TextField
+                margin="normal"
+                name="confirmpassword"
+                label="confirmpassword"
+                type="password"
+                value={confpassword2}
+                onChange={(event) => setConfPassword(event.target.value)}
+              />
+              <MuiSelect
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={privilege}
+              >
+                <MuiMenuItem value={'Administrador'}>Administrador</MuiMenuItem>
+                <MuiMenuItem value={'Psicologo'}>Psicólogo</MuiMenuItem>
+              </MuiSelect>
+              <div>
+                <Button type="submit" variant="contained" color="primary" onClick={() => { onSubmit() }}>
+                  Cambiar
+                </Button>
+                <Button type="submit" variant="contained" color="primary" onClick={() => { setEdit(!edit) }}>
+                  Cancelar
+                </Button>
+              </div>
+          </FormControl>
+          }
+          </div>
+    );}
+
+export default UserDisplay;
--- src/views/AccessModule.tsx
+++ src/views/AccessModule.tsx
@@ -1,16 +1,71 @@
-import React from 'react';
+import React, {useState, MouseEvent} from 'react';
 /** @jsx jsx */ import { css, jsx } from '@emotion/core';
+import Button from '@material-ui/core/Button';
+import UserDisplay from '../components/UserDisplay'
 
 const styledTitle = css({
   textAlign: 'center',
 });
 
+const divStyle = css({
+  padding: '10px',
+  margin: '20px',
+  borderRadius: '10px',
+  border: '1px solid black',
+  display:'flex',
+  maxWidth: '50vw',
+  '@media (max-width: 600px)': {
+    maxWidth: '80vw',
+  },
+  justifyContent:'space-between'
+});
+
+
+
+const h1Style = css({
+  margin: '5px 0px',
+});
+
+const users = [{
+  userID: '1',
+  username :  "usuario1",
+  privilege: '1',
+  password: "password1"
+
+},{
+  userID: '2',
+  username :  "usuario2",
+  privilege: '2',
+  password: "password2"
+
+}]
+
+
+
+
 const AccessModule: React.FC = () => {
+  const [edit, setEdit] = useState(false);
   return (
     <div>
-      <h1 css={styledTitle}>Módulo de acceso</h1>
+      <h1 css={styledTitle}>
+        Módulo de acceso</h1>
+      <div>
+        {users.map((u)=>(
+            <div css={divStyle}>
+                <h1 css={h1Style}>
+                    {u.username}
+                </h1>
+                <UserDisplay username={u.username} password={u.password} privilege={u.privilege} />
+            </div>
+            
+        ))}
+      </div>
+      
     </div>
   );
 };
+
+
+
 
 export default AccessModule;
