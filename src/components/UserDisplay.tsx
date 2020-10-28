2a9178a97f3975c89919d58536234ee357deeb49
GinaJame <gina10111@hotmail.com>
2020-10-28 01:10:29 -0600

    AccessModule changes
    Show users

--- src/components/UserDisplay.tsx
+++ src/components/UserDisplay.tsx
@@ -1,22 +1,18 @@
-import React from 'react';
+
+import React, {useState, MouseEvent} from 'react';
+import Markdown from 'markdown-to-jsx';
 import Button from '@material-ui/core/Button';
-import useMediaQuery from '@material-ui/core/useMediaQuery';
-import { useTheme } from '@material-ui/core/styles';
-import Dialog from '@material-ui/core/Dialog';
-import DialogActions from '@material-ui/core/DialogActions';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-//import resetPasswordResidentFamAcc from '../../functions/src/functions/resetPasswordResidentFamAcc';
-import {functions} from '../firebase/app'
+import TextField from '@material-ui/core/TextField';
+import MuiSelect from '@material-ui/core/Select';
+import MuiMenuItem from '@material-ui/core/MenuItem';
+import { FormControl } from "@material-ui/core";
 /** @jsx jsx */ import { css, jsx } from '@emotion/core';
-//import { AnalyticsEvent } from 'firebase-functions/lib/providers/analytics';
 
 
 const divStyle = css({
   padding: '10px',
   margin: '20px',
-  borderRadius: '10px',
-  border: '1px solid black',
+
   display:'flex',
   maxWidth: '50vw',
   '@media (max-width: 600px)': {
@@ -31,59 +27,71 @@ const h1Style = css({
   margin: '5px 0px',
 });
 
-
-
-//const UserDisplay: React.FC <{key: string, firstName: string, lastName: string}>= ({key, firstName, lastName}) => {
-  const UserDisplay: React.FC <{  resident:any}>= ({ resident}) => {
-    const [open, setOpen] = React.useState(false);
-
-    const handleClose = () => {
-      setOpen(false);
-    };
-
-    const theme = useTheme();
-    const fullScreen = useMediaQuery(theme.breakpoints.down('sm'));
-    const resID = resident.residentID;
-    console.log(resID);
-
-
+const UserDisplay: React.FC <{username: string, password:string, privilege:string}>= ({username, password, privilege}) => {
+    const [edit, setEdit] = useState(false);
+    const [username2, setUsername] = useState(username);
+    const [password2, setPassword] = useState(password);
+    const [confpassword2, setConfPassword] = useState('');
+    const [privilege2, setPrivilege] = useState(privilege);
     const onSubmit = () => {
-      const resetPasswordResidentFamAcc = functions.httpsCallable('resetPasswordResidentFamAcc');
-      setOpen(true)
-      /*resetPasswordResidentFamAcc({uid:resID}).then((value) =>{
-        if ((value as unknown as SuccessMessage).success) {
-          setOpen(true);
-        }
-      });*/
+      if(confpassword2==password2){
+        console.log('Iniciando sesión con datos:', username2, password2,privilege2);
+      }
     };
     return(
         <div css={divStyle}>
-          
-              <h1 css={h1Style}>
-                  {resident.firstName} {"  "}  {resident.lastName}
-              </h1>
-              <Button type="submit" variant="contained" color="primary" onClick={() => { onSubmit() }}>
-                Cambiar
-              </Button>
-              <Dialog
-                fullScreen={fullScreen}
-                open={open}
-                onClose={handleClose}
-                aria-labelledby="responsive-dialog-title"
+          {!edit &&
+          <Button type="submit" variant="contained" color="primary" onClick={() => { setEdit(!edit) }}>
+                    Cambiar
+          </Button>
+          }
+          { edit &&
+          <FormControl>
+              <TextField
+                  margin="normal"
+                  id="username"
+                  label={username}
+                  name="username"
+                  autoFocus
+                  value={username2}
+                  onChange={(event) => setUsername(event.target.value)}
+                />
+              <TextField
+                margin="normal"
+                name="password"
+                label="password"
+                type="password"
+                id={password}
+                value={password2}
+                onChange={(event) => setPassword(event.target.value)}
+              />
+              <TextField
+                margin="normal"
+                name="confirmpassword"
+                label="confirmpassword"
+                type="password"
+                value={confpassword2}
+                onChange={(event) => setConfPassword(event.target.value)}
+              />
+              <MuiSelect
+                labelId="demo-simple-select-label"
+                id="demo-simple-select"
+                value={privilege}
               >
-                <DialogContent>
-                  <DialogContentText>
-                    La contraseña fue cambiada correctamente
-                  </DialogContentText>
-                </DialogContent>
-                <DialogActions>
-                  <Button autoFocus onClick={handleClose} color="primary">
-                    Okay
-                  </Button>
-                </DialogActions>
-              </Dialog>
+                <MuiMenuItem value={'Administrador'}>Administrador</MuiMenuItem>
+                <MuiMenuItem value={'Psicologo'}>Psicólogo</MuiMenuItem>
+              </MuiSelect>
+              <div>
+                <Button type="submit" variant="contained" color="primary" onClick={() => { onSubmit() }}>
+                  Cambiar
+                </Button>
+                <Button type="submit" variant="contained" color="primary" onClick={() => { setEdit(!edit) }}>
+                  Cancelar
+                </Button>
+              </div>
+          </FormControl>
+          }
           </div>
-    );
-  }
+    );}
 
 export default UserDisplay;
