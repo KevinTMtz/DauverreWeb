734f66829f63bcc0363b66d4ceed979d7ad35a04
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2020-10-30 14:37:44 -0600

    Report List Page added

--- src/views/resident/ViewResidentPage.tsx
+++ src/views/resident/ViewResidentPage.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 
 import PageTitle from '../../components/PageTitle';
+import ReportsRouter from '../report';
 
 const ViewResidentPage: React.FC = () => {
   const { residentID } = useParams<ResidentParams>();
@@ -9,6 +10,7 @@ const ViewResidentPage: React.FC = () => {
     <div>
       <PageTitle message={'Residente'} />
       <h3>Viewing resident "{residentID}"</h3>
+      <ReportsRouter />
     </div>
   );
 };
