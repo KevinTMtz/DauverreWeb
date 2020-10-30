38db8902733381734d77225f9a5318b5879af734
MarioJim <mario.emilio.j@gmail.com>
2020-10-29 22:10:00 -0600

    Login Page now works

--- src/firebase/auth.ts
+++ src/firebase/auth.ts
@@ -1,7 +1,33 @@
+import { auth } from './app';
+
 export const signInWithCredentials = async (
   username: string,
   password: string,
-): Promise<{ user: string } | { error: string }> => {
-  if (password === 'uwu') return { user: username || 'soy admin' };
-  return { error: 'Usuario o contraseña no válidos' };
+): Promise<SuccessMessage | { error: string }> => {
+  const email = username.includes('@') ? username : `${username}@example.com`;
+  try {
+    const userCred = await auth.signInWithEmailAndPassword(email, password);
+    const token = await userCred.user?.getIdTokenResult();
+    if (token?.claims.admin) return { success: true, url: '/menu' };
+    return { success: true, url: `/residents/${userCred.user?.uid}` };
+  } catch (error) {
+    const authMethod = username.includes('@')
+      ? 'correo electrónico'
+      : 'teléfono';
+    switch (error.code) {
+      case 'auth/invalid-email':
+        return { error: 'Usuario no válido' };
+      case 'auth/user-disabled':
+        return { error: 'Usuario deshabilitado' };
+      case 'auth/user-not-found':
+        return {
+          error: `No existe un usuario con ese ${authMethod}`,
+        };
+      case 'auth/wrong-password':
+        return { error: 'Contraseña incorrecta' };
+      default:
+        console.error(error);
+        return { error: 'Error en el servidor' };
+    }
+  }
 };
