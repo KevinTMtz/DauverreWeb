07824d9077ad34a6de12e450e5ae03fc9cc650aa
MarioJim <mario.emilio.j@gmail.com>
2020-10-29 22:28:36 -0600

    Create separate page for global and weekly report

---
+++ src/views/GlobalReportPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import PageTitle from '../components/PageTitle';
+
+const GlobalReportPage: React.FC = () => (
+  <div>
+    <PageTitle message={'Reporte global'} />
+  </div>
+);
+
+export default GlobalReportPage;
