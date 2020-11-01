3492a8f7fc42f4e95952acbf01785aacb55b7f38
GinaJame <gina10111@hotmail.com>
2020-11-01 02:37:35 -0600

    exportar functions de app a user display

--- src/firebase/app.ts
+++ src/firebase/app.ts
@@ -1,5 +1,6 @@
 import firebase from 'firebase/app';
 import 'firebase/firestore';
+import 'firebase/functions'
 
 const firebaseConfig = {
   apiKey: 'AIzaSyDPm0dMe1rpGOgV6Hkgk5v6E0kujBhTiEU',
@@ -16,6 +17,7 @@ const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 export default firebaseApp;
 
-//export const functions = firebaseApp.functions();
 
 export const db = firebaseApp.firestore();
+
+export const functions = firebaseApp.functions();
