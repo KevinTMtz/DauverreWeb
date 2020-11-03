31088fcf55c5214a45c39740d0c1878fef8e73f4
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-11-03 13:48:35 -0600

    Added Firebase file storage functions.

--- src/react-app-env.d.ts
+++ src/react-app-env.d.ts
@@ -55,6 +55,10 @@ interface NotFoundError {
   notFound: boolean;
 }
 
+interface UnableToUploadFile {
+  notFound: boolean;
+}
+
 type ValidationErrors = string[];
 
 type FirebaseCollection = firebase.firestore.CollectionReference<
