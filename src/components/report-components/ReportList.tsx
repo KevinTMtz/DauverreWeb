84aa93e94b9f95eaf6bbf5b6f481d055c3dd4a0c
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2020-11-02 23:48:31 -0600

    Issues resolved

--- src/components/report-components/ReportList.tsx
+++ src/components/report-components/ReportList.tsx
@@ -1,27 +1,29 @@
 import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import CircularProgress from '@material-ui/core/CircularProgress';
 
 import { getReports } from '../../firebase/db/reports';
 import ReportListCell from './ReportListCell';
 
 const ReportsList: React.FC = () => {
   const { residentID } = useParams<ResidentParams>();
-  const [reports, setReports] = useState<Report[]>([]);
+  const [reports, setReports] = useState<Report[]>();
   useEffect(() => {
-    getReports(residentID).then((repid) => setReports(repid));
+    getReports(residentID).then((repid) => {
+      repid.reverse();
+      setReports(repid);
+    });
   }, [residentID]);
   return (
     <div>
       <h1 style={{ textAlign: 'center' }}>Últimos reportes</h1>
-      {reports.length === 0 ? (
-        /*No sé como podría ponerle, porque quiero que esto aparezca 
-        cuando no hay reportes pero aparecerá mintras carga la base de datos*/
+      {typeof reports === 'undefined' ? (
         <h1 style={{ textAlign: 'center' }}>No hay reportes</h1>
+      ) : reports.length === 0 ? (
+        // No sé porque no está funcionando
+        <CircularProgress style={{ margin: 'auto' }} />
       ) : (
-        reports
-          .slice(0)
-          .reverse()
-          .map((r) => <ReportListCell key={r.reportID} {...r} />)
+        reports.map((r) => <ReportListCell key={r.reportID} {...r} />)
       )}
     </div>
   );
