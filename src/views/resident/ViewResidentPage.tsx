f063855a7468ab52fb494e61a91846d82ea4ef8a
SebasRod23 <sebastian_rodriguezg@yahoo.com>
2020-11-16 01:01:45 -0600

    ViewReports button added

--- src/views/resident/ViewResidentPage.tsx
+++ src/views/resident/ViewResidentPage.tsx
@@ -1,17 +1,41 @@
-import React from 'react';
-import { useParams, Route, Switch, useRouteMatch } from 'react-router-dom';
+import React, { useEffect, useState } from 'react';
+import {
+  useParams,
+  Route,
+  Switch,
+  useRouteMatch,
+  useHistory,
+} from 'react-router-dom';
 
 import PageTitle from '../../components/PageTitle';
 import CreateReport from '../../components/report-components/CreateReport';
 import ReportsList from '../../components/report-components/ReportList';
+import { getResident } from '../../firebase/db/residents';
 
 const ViewResidentPage: React.FC = () => {
   const { residentID } = useParams<ResidentParams>();
+  const history = useHistory();
+  const [resident, setResident] = useState<ResidentData>({
+    firstName: 'Cargando...',
+    lastName: '',
+    gender: '',
+    isVisible: true,
+    birthDate: new Date(),
+  });
+  useEffect(() => {
+    getResident(residentID).then((value) => {
+      if (value.state != 'not found') {
+        setResident(value.resident);
+      }
+    });
+  }, [residentID, history]);
   const match = useRouteMatch();
   return (
     <div>
-      <PageTitle message={'Residente'} />
-      <h3>Viewing resident "{residentID}"</h3>
+      <PageTitle message={'Residente:'} />
+      <h3 style={{ textAlign: 'center' }}>
+        {resident.firstName + ' ' + resident.lastName}
+      </h3>
       <Switch>
         <Route path={`${match.path}/newreport`}>
           <CreateReport />
