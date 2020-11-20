8a839bf54f608ec975f0b427503ac149ce09f399
MarioJim <mario.emilio.j@gmail.com>
2020-11-19 20:44:25 -0600

    Move deleteResident to src/firebase/functions.ts

--- src/views/resident/ResidentListPage.tsx
+++ src/views/resident/ResidentListPage.tsx
@@ -1,12 +1,19 @@
 import { useHistory, useRouteMatch } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 /** @jsx jsx */ import { css, jsx } from '@emotion/core';
+import Button from '@material-ui/core/Button';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogActions from '@material-ui/core/DialogActions';
+import Dialog from '@material-ui/core/Dialog';
 
+import CircularProgressIndicator from '../../components/CircularProgressIndicator';
 import PageTitle from '../../components/PageTitle';
 import ResidentListCell from '../../components/resident-components/ResidentListCell';
 import SearchInput from '../../components/resident-components/ResidentSearch';
 
-import { getResidents, deleteResident } from '../../firebase/db/residents';
+import { getResidents } from '../../firebase/db/residents';
+import { deleteResident } from '../../firebase/functions';
 
 const addButtonStyle = css({
   width: '70%',
@@ -38,6 +45,7 @@ const ResidentListPage: React.FC = () => {
 
   const [residents, setResidents] = useState<Resident[]>([]);
   const [displayedResidents, setDisplayedResidents] = useState<Resident[]>([]);
+  const [formState, setFormState] = useState<FormState>({ state: 'closed' });
   useEffect(() => {
     getResidents().then((resid) => {
       setDisplayedResidents(resid);
@@ -46,8 +54,20 @@ const ResidentListPage: React.FC = () => {
   }, []);
 
   const deleteSelectedResident = (residentID: string) => {
-    deleteResident(residentID).then(() => {
-      setResidents(residents.filter((r) => r.residentID !== residentID));
+    setFormState({ state: 'loading' });
+    deleteResident(residentID).then((resp) => {
+      if (resp.state === 'success') {
+        setResidents(residents.filter((r) => r.residentID !== residentID));
+        setDisplayedResidents(
+          displayedResidents.filter((r) => r.residentID !== residentID),
+        );
+        setFormState({ state: 'closed' });
+      } else {
+        setFormState({
+          state: 'server error',
+          message: 'El servidor tiene problemas con tu solicitud',
+        });
+      }
     });
   };
 
@@ -71,6 +91,31 @@ const ResidentListPage: React.FC = () => {
           {...dispResident}
         />
       ))}
+      <Dialog
+        disableBackdropClick
+        disableEscapeKeyDown
+        open={formState.state !== 'closed'}
+      >
+        {formState.state === 'loading' && (
+          <DialogContent>
+            <CircularProgressIndicator />
+          </DialogContent>
+        )}
+        {formState.state === 'server error' && (
+          <React.Fragment>
+            <DialogTitle>Error</DialogTitle>
+            <DialogContent>{formState.message}</DialogContent>
+            <DialogActions>
+              <Button
+                onClick={() => setFormState({ state: 'closed' })}
+                color="primary"
+              >
+                Ok
+              </Button>
+            </DialogActions>
+          </React.Fragment>
+        )}
+      </Dialog>
     </div>
   );
 };
