d26adff6e29f3e0c6acd22ac9b44c6c0d8eb9202
Kevin Torres Martínez <torresmtzkevin@gmail.com>
2020-11-21 21:20:04 -0600

    Added button to restart stats of the operations count.

--- src/firebase/db/stats.ts
+++ src/firebase/db/stats.ts
@@ -8,12 +8,44 @@ export const increment = (incrementNum: number) =>
 export const decrement = (decrementNum: number) =>
   firestore.FieldValue.increment(decrementNum * -1);
 
-export const getStatsDoc = async (): Promise<SuccessAndStats> => {
-  const collection = await statsCollection.get();
-  return {
-    state: 'success',
-    statsCollection: collection.docs.map((doc) => {
-      return { statsName: doc.id, ...doc.data() };
-    }),
-  };
+export const getStatsDoc = async (): Promise<
+  SuccessAndStats | FirebaseErrorState
+> => {
+  try {
+    const collection = await statsCollection.get();
+    return {
+      state: 'success',
+      statsCollection: collection.docs.map((doc) => {
+        return { statsName: doc.id, ...doc.data() };
+      }),
+    };
+  } catch (error) {
+    return {
+      state: 'firebase error',
+      code: error.code,
+      message: error.message,
+    };
+  }
+};
+
+export const restartOperationsCount = async (): Promise<
+  SuccessState | FirebaseErrorState
+> => {
+  try {
+    await statsCollection
+      .doc('postsOperationsCount')
+      .update({ deletions: 0, registrations: 0, updates: 0 });
+    await statsCollection
+      .doc('residentsOperationsCount')
+      .update({ deletions: 0, registrations: 0, updates: 0 });
+    return {
+      state: 'success',
+    };
+  } catch (error) {
+    return {
+      state: 'firebase error',
+      code: error.code,
+      message: error.message,
+    };
+  }
 };
