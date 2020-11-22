a542283a566bbab3163dcd60c60e2c959383f5dd
MarioJim <mario.emilio.j@gmail.com>
2020-11-21 18:00:43 -0600

    Show spinner while loading accounts in ResidentForm

--- src/components/CircularProgressIndicator.tsx
+++ src/components/CircularProgressIndicator.tsx
@@ -1,12 +1,20 @@
 import React from 'react';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
-const CircularProgressIndicator: React.FC = () => (
+interface CircularProgressIndicatorProps {
+  size?: string;
+  margin?: string;
+}
+
+const CircularProgressIndicator: React.FC<CircularProgressIndicatorProps> = ({
+  size,
+  margin,
+}) => (
   <CircularProgress
     style={{
-      width: '75px',
-      height: '75px',
-      margin: '25px',
+      width: size || '75px',
+      height: size || '75px',
+      margin: margin || '25px',
       color: '#74b9ff',
     }}
   />
